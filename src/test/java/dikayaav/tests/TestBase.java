8d3e8a1311b048cb1786de9eac293c11db879190
SashkaDikaya <dikayaav@gmail.com>
2022-02-07 22:54:27 +0300

    add 3 tests: changeLanguageTest, checkPdfTest, checkSuccessStoryTest

---
+++ src/test/java/dikayaav/tests/TestBase.java
@@ -0,0 +1,46 @@
+package dikayaav.tests;
+
+import com.codeborne.selenide.Configuration;
+import dikayaav.helper.Attach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class TestBase {
+
+    @BeforeAll
+    static void setUp() {
+        String browser = System.getProperty("browser", "chrome");
+        String version = System.getProperty("version", "91");
+        String size = System.getProperty("size", "1920x1080");
+        String remoteUrl = System.getProperty("remoteUrl", "selenoid.autotests.cloud/wd/hub");
+        String login = System.getProperty("login", "user1");
+        String pass = System.getProperty("pass", "1234");
+
+        Configuration.baseUrl = "https://demoqa.com";
+
+        String url = "https://" + login + ":" + pass + "@" + remoteUrl;
+        Configuration.remote = url;
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+
+        Attach.attachAsText("Browser: ", browser);
+        Attach.attachAsText("Version: ", version);
+        Attach.attachAsText("Remote Url: ", remoteUrl);
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
+    }
+
+}
---
+++ src/test/java/dikayaav/tests/VeeamTests.java
@@ -0,0 +1,76 @@
+package dikayaav.tests;
+
+import com.codeborne.pdftest.PDF;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.File;
+
+import static com.codeborne.pdftest.assertj.Assertions.assertThat;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$x;
+import static io.qameta.allure.Allure.step;
+
+
+public class VeeamTests extends TestBase {
+
+    @ValueSource(strings = {"Biocad", "Exchange"})
+    @ParameterizedTest(name = "Открытие статьи о : {0}")
+    void checkSuccessStoryTest(String customer) {
+        step("Открытие главной страницы сайта Veeam", () -> {
+            Selenide.open("https://www.veeam.com/ru");
+        });
+
+        step("Открытие историй успеха заказчиков", () -> {
+            $x("//a[text()='Истории успеха заказчиков']").click();
+        });
+
+        step("Поиск информации о " + customer, () -> {
+            $x("//span[contains(text(),'Показать поле поиска')]").click();
+            $x("//input[@class='form-input search-input']").setValue(customer).pressEnter();
+        });
+
+        step("Проверка наличия статьи о " + customer, () -> {
+            $x("//div[@class='success-stories-listing__listing']").shouldHave(text(customer));
+        });
+
+        step("Открытие первой статьи о " + customer, () -> {
+            $x("//a[text()='Подробнее']").click();
+        });
+
+        step("Проверка наличия информации в статье о " + customer, () -> {
+            $x("//*").shouldHave(text(customer));
+        });
+    }
+
+    @Test
+    @DisplayName("Поиск информации о компании Biocad в скачанном PDF файле")
+        void checkPdfTest() throws Exception {
+            step("Поиск информации о компании Biocad в скачанном PDF файле", () -> {
+                Selenide.open("https://www.veeam.com/ru/success-stories/Biocad.html");
+                File downloadedPdfFile = $x("//span[contains(text(), 'Скачать PDF')]/..").download();
+                PDF parsed = new PDF(downloadedPdfFile);
+                assertThat(parsed.title).contains("Veeam", "Biocad");
+            });
+    }
+
+    @Test
+    @DisplayName("Смена языка с русского на английский")
+    void changeLanguageTest()  {
+        step("Смена языка с русского на английский", () -> {
+            Selenide.open("https://www.veeam.com/ru");
+            $x("//div[@class='language-selector']").click();
+            $x("//a[@data-lang='en']").click();
+            $x("//a[@class='additional-info__link contact-sales-link']").shouldHave(text("Contact Sales"));
+        });
+    }
+
+
+
+}
+
+
+
